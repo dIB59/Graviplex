75222db24ee7644f5119c871bfb4d1c193be53ca
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-07-17 21:36:45 +0200

    update deprecated code

--- src/fps.rs
+++ src/fps.rs
@@ -122,17 +122,17 @@ fn fps_text_update_system(
             // text according to the FPS value:
             text.sections[1].style.color = if value >= 120.0 {
                 // Above 120 FPS, use green color
-                Color::rgb(0.0, 1.0, 0.0)
+                Color::srgb(0.0, 1.0, 0.0)
             } else if value >= 60.0 {
                 // Between 60-120 FPS, gradually transition from yellow to green
-                Color::rgb(
+                Color::srgb(
                     (1.0 - (value - 60.0) / (120.0 - 60.0)) as f32,
                     1.0,
                     0.0,
                 )
             } else if value >= 30.0 {
                 // Between 30-60 FPS, gradually transition from red to yellow
-                Color::rgb(
+                Color::srgb(
                     1.0,
                     ((value - 30.0) / (60.0 - 30.0)) as f32,
                     0.0,
