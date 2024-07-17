9fb9420fcd7149e1d45aebdd34db332f755a45ab
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-07-17 23:19:58 +0200

    particle is now a circle

--- src/particle.rs
+++ src/particle.rs
@@ -1,5 +1,6 @@
 use bevy::math::vec3;
 use bevy::prelude::*;
+use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 use rand::Rng;
 use crate::movement::Velocity;
 
@@ -11,29 +12,30 @@ impl Plugin for ParticlePlugin {
     }
 }
 
-// #[derive(Component)]
-// pub struct Particle;
+#[derive(Component)]
+pub struct Particle;
 
-fn spawn_particle(mut commands: Commands) {
+fn spawn_particle(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
     let mut rng = rand::thread_rng();
     let random_velocity = Vec2::new(rng.gen_range(-1.0..1.0), rng.gen_range(-1.0..1.0));
 
     commands.spawn((
-        SpriteBundle {
+        MaterialMesh2dBundle {
             transform: Transform {
                 translation: vec3(0., 0., 0.),
                 ..default()
             },
-            sprite: Sprite {
-                color: Color::srgb(0.7, 0.3, 0.7),
-                // custom_size: Some(Vec2::new( 2.0, 2.0)),
-                ..default()
-            },
+            mesh: Mesh2dHandle(meshes.add(Circle { radius: 10.0 })),
+            material: materials.add(Color::WHITE),
             ..default()
         },
-        // Particle,
         Velocity {
             value: random_velocity,
         },
+        Particle,
     ));
 }
