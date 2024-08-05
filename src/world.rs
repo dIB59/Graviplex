cc6a960ab061558a71f66c532c9d0895750308b4
Ibrahim <Ibrahim@ebs.net.pk>
2024-08-05 15:03:16 +0200

    refactor: handle cursor pos error

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,4 @@
-use bevy::log::info;
+use bevy::log::{error, info};
 use bevy::math::Vec2;
 use bevy::prelude::{Camera, GlobalTransform, Window};
 
@@ -9,9 +9,17 @@ pub fn camera_to_world_coordinate(
 ) -> Option<Vec2> {
     info!("ORIGINAL: {:?}", window.cursor_position());
 
-    let pos = camera.viewport_to_world_2d(camera_transform, window.cursor_position()?);
+    let cursor_pos = window.cursor_position();
 
-    info!("Transformed: {:?}", pos);
-
-    pos
+    match cursor_pos {
+        Some(pos) => {
+            let world_pos = camera.viewport_to_world_2d(camera_transform, pos);
+            info!("Transformed world position: {:?}", world_pos);
+            world_pos
+        }
+        None => {
+            error!("Cursor position is not available in the window.");
+            cursor_pos
+        }
+    }
 }
