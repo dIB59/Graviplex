1259d7dd6e95d17277f180738df271928a3fd1fb
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2024-12-30 15:30:10 +0100

    wip Surface::configure: Validation Error

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
 use std::sync::Arc;
 
+use env_logger::fmt::style::Color;
+use pollster::FutureExt;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::ActiveEventLoop;
 use winit::window::{Window, WindowId};
 
-#[derive(Default)]
+#[derive(Default, Debug, Clone)]
 pub struct App {
     window: Arc<Option<Window>>,
 }
@@ -19,6 +21,91 @@ impl ApplicationHandler for App {
                 .expect("create window err.");
             self.window = Arc::new(Some(window))
         }
+
+        let size = self
+            .window
+            .as_ref()
+            .as_ref()
+            .expect("No window found")
+            .inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+
+        let surface = instance
+            .create_surface(
+                self.window
+                    .as_ref()
+                    .as_ref()
+                    .expect("Window not found for surface"),
+            )
+            .expect("Unable to create surface");
+
+        let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        }))
+        .expect("Unable to create adapter");
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_limits: wgpu::Limits::default(),
+                    ..Default::default()
+                },
+                None,
+            )
+            .block_on()
+            .expect("Unable to create device");
+
+        let format: wgpu::TextureFormat = surface.get_capabilities(&adapter).formats[0];
+
+        let mut surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Mailbox,
+            format,
+            desired_maximum_frame_latency: Default::default(),
+            alpha_mode: Default::default(),
+            view_formats: Default::default(),
+        };
+
+        surface.configure(&device, &surface_config);
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let render_pipline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &[],
+                compilation_options: Default::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: Default::default(),
+                compilation_options: Default::default(),
+            }),
+            primitive: wgpu::PrimitiveState::default(),
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState::default(),
+            multiview: Default::default(),
+            cache: Default::default(),
+        });
     }
 
     fn window_event(
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,22 +1,15 @@
-struct VertexInput {
-    @builtin(position) position: vec4f,
-};
-
-struct VertexOutput {
-   @builtin(position) position: vec4f,
-   @location(0) color : vec3f
-}
-
-@vertex
-fn vs_main(vertex_input: VertexInput) -> VertexOutPut {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x , y , 1.0, 1.0);
+[[stage(vertex)]]
+fn vs_main([[builtin(vertex_index)] in_vertex_index: u32) -> [[builtin(position)]] vec4<f32> {
+    var pos = array<vec2<f32>, 3>(
+        vec2<f32>(0.0, 0.5),
+        vec2<f32>(-0.5, -0.5),
+        vec2<f32>(0.5, -0.5)
+        );
+    return vec4<f32>(pos[in_vertex_index], 0.0, 1.0);
 
 }
 
-//color of triangle
-@fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+[[stage(fragment)]]
+fn fs_main() -> [[location(0)]] vec4<f32> {
+    return vec4<f32>(1.0, 1.0, 0.0, 1.0);
 }
